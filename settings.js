df881541da157c1b1efcf986bad463d20e2bb251
Festus4 <festusmuasya4@gmail.com>
2024-10-15 15:05:58 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,25 +4,25 @@
 //Instagram: techgod143
 //Telegram: t.me/techgod143
 //GitHub: @techgod143
-//WhatsApp: +917466008456
+//WhatsApp: +254106515408
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@techgod143
 const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
+global.ownernomer = "254106515408"
 global.ownername = "🐛Tech God"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
-global.location = "India, Up, Rampur"
+global.location = "Kenya, Nairobi"
 
-global.ownernumber = '917466008456'  //creator number
+global.ownernumber = '254106515408'  //creator number
 global.ownername = '🐛Tech god' //owner name
 global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'Tech-God\n\nContact: +917466008456'
+global.author = 'Tech-God\n\nContact: +254106515408'
 
 //console view/theme
 global.themeemoji = '🪀'
@@ -35,12 +35,12 @@ global.link = 'https://whatsapp.com/channel/0029Va9Ufzi8kyyEnEHvOm1h'
 global.prefa = ['','!','.','#','&']
 
 //false=disable and true=enable
-global.autoRecording = false //auto recording
-global.autoTyping = false //auto typing
+global.autoRecording = true //auto recording
+global.autoTyping = true //auto typing
 global.autorecordtype = true //auto typing + recording
-global.autoread = false //auto read messages
+global.autoread = true //auto read messages
 global.autobio = true //auto update bio
-global.anti92 = false //auto block +92 
+global.anti254 = true //auto block +254 
 global.autoswview = true //auto view status/story
 
 //menu type 
